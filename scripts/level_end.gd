358b153da94c178e9d37add516f2dff2a30eb41f
gustavo-ouriques <guouriques06@gmail.com>
2026-07-16 03:14:21 -0300

    levels atualizados

--- scripts/level_end.gd
+++ scripts/level_end.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 @export var next_level = ""
+@export var game_win = ""
+
 
 func _on_body_entered(_body: Node2D) -> void:
 	call_deferred("load_next_scene")
@@ -8,3 +10,6 @@ func _on_body_entered(_body: Node2D) -> void:
 	
 func load_next_scene():
 	get_tree().change_scene_to_file("res://cenas/" + next_level + ".tscn")
+
+func load_game_win():
+	get_tree().change_scene_to_file("res://menu/vitoria.tscn")
